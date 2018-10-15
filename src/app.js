3225f4c387a4cf91b362d8b77b2fa5e144db6add
ElizabethVegaM <eb.vega.m@gmail.com>
2018-10-15 00:36:36 -0300

    menu responsive con detalles, animaciones, css, etc

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
-//Using Vanilla JS
-// document.querySelector(".hamburguer").addEventListener("click", function(){
-// 	document.querySelector(".full-menu").classList.toggle("active");
-// 	document.querySelector(".hamburguer").classList.toggle("close-hamburguer");
-// });
+AOS.init();
+
+$(".toggle").click(function() {
+  $(".nav-bar").slideToggle();
+$(this).toggleClass("is-active");
+});
+$(".dropdown").click(function() {
+  $(".sub-nav").slideToggle();
+  $(this).toggleClass("active");
+});
+
