4c9407bd8294e437b395a7c46c645640ac2cba22
ElizabethVegaM <eb.vega.m@gmail.com>
2018-10-17 18:40:47 -0300

    arreglos finales

--- src/app.js
+++ src/app.js
@@ -1,76 +1,119 @@
 AOS.init();
 
-$(".toggle").click(function () {
+$(".toggle").click(function() {
   $(".nav-bar").slideToggle();
-  $(this).toggleClass("is-active");
+$(this).toggleClass("is-active");
 });
-$(".dropdown").click(function () {
+$(".dropdown").click(function() {
   $(".sub-nav").slideToggle();
   $(this).toggleClass("active");
 });
 
-// ------------- VARIABLES ------------- //
-var ticking = false;
-var isFirefox = (/Firefox/i.test(navigator.userAgent));
-var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
-var slideDurationSetting = 600; //Amount of time for which slide is "locked"
-var currentSlideNumber = 0;
-var totalSlideNumber = $(".background").length;
+/* Stars animation */
 
-// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
-function parallaxScroll(evt) {
-  if (isFirefox) {
-    //Set delta for Firefox
-    delta = evt.detail * (-120);
-  } else if (isIe) {
-    //Set delta for IE
-    delta = -evt.deltaY;
-  } else {
-    //Set delta for all other browsers
-    delta = evt.wheelDelta;
-  }
+var canvas;
+var context;
+var screenH;
+var screenW;
+var stars = [];
+var fps = 60;
+var numStars = 500;
 
-  if (ticking != true) {
-    if (delta <= -scrollSensitivitySetting) {
-      //Down scroll
-      ticking = true;
-      if (currentSlideNumber !== totalSlideNumber - 1) {
-        currentSlideNumber++;
-        nextItem();
-      }
-      slideDurationTimeout(slideDurationSetting);
-    }
-    if (delta >= scrollSensitivitySetting) {
-      //Up scroll
-      ticking = true;
-      if (currentSlideNumber !== 0) {
-        currentSlideNumber--;
-      }
-      previousItem();
-      slideDurationTimeout(slideDurationSetting);
-    }
-  }
+$('document').ready(function() {
+  
+  // Calculate the screen size
+	screenH = $(window).height();
+	screenW = $(window).width();
+	
+	// Get the canvas
+	canvas = $('#space');
+	
+	// Fill out the canvas
+	canvas.attr('height', screenH);
+	canvas.attr('width', screenW);
+	context = canvas[0].getContext('2d');
+	
+	// Create all the stars
+	for(var i = 0; i < numStars; i++) {
+		var x = Math.round(Math.random() * screenW);
+		var y = Math.round(Math.random() * screenH);
+		var length = 1 + Math.random() * 1.5;
+		var opacity = Math.random();
+		
+		// Create a new star and draw
+		var star = new Star(x, y, length, opacity);
+		
+		// Add the the stars array
+		stars.push(star);
+	}
+	
+	animateInterval = setInterval(animate, 1000 / fps);
+});
+
+/**
+ * Animate the canvas
+ */
+function animate() {
+	context.clearRect(0, 0, screenW, screenH);
+	$.each(stars, function() {
+		this.draw(context);
+	})
 }
 
-// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
-function slideDurationTimeout(slideDuration) {
-  setTimeout(function () {
-    ticking = false;
-  }, slideDuration);
+/* stop Animation */
+function stopAnimation()
+{
+     clearInterval(animateInterval);
 }
 
-// ------------- ADD EVENT LISTENER ------------- //
-var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
-window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+//stopAnimation();
 
-// ------------- SLIDE MOTION ------------- //
-function nextItem() {
-  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
-  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+function Star(x, y, length, opacity) {
+	this.x = parseInt(x);
+	this.y = parseInt(y);
+	this.length = parseInt(length);
+	this.opacity = opacity;
+	this.factor = 1;
+	this.increment = Math.random() * .03;
 }
 
-function previousItem() {
-  var $currentSlide = $(".background").eq(currentSlideNumber);
-  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+Star.prototype.draw = function() {
+	context.rotate((Math.PI * 1 / 10));
+	
+	// Save the context
+	context.save();
+	
+	// move into the middle of the canvas, just to make room
+	context.translate(this.x, this.y);
+	
+	// Change the opacity
+	if(this.opacity > 1) {
+		this.factor = -1;
+	}
+	else if(this.opacity <= 0) {
+		this.factor = 1;
+		
+		this.x = Math.round(Math.random() * screenW);
+		this.y = Math.round(Math.random() * screenH);
+	}
+		
+	this.opacity += this.increment * this.factor;
+	
+	context.beginPath()
+	for (var i = 5; i--;) {
+		context.lineTo(0, this.length);
+		context.translate(0, this.length);
+		context.rotate((Math.PI * 2 / 10));
+		context.lineTo(0, - this.length);
+		context.translate(0, - this.length);
+		context.rotate(-(Math.PI * 6 / 10));
+	}
+	context.lineTo(0, this.length);
+	context.closePath();
+	context.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
+	context.shadowBlur = 5;
+	context.shadowColor = '#fff';
+	context.fill();
+	
+	context.restore();
 }
