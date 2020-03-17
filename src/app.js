8ed92573831f4d20ca12b28962af86de598a1ea8
Elizabeth Vega <eb.vega.m@gmail.com>
2020-03-17 18:19:25 -0300

    navbar ready

--- src/app.js
+++ src/app.js
@@ -1,66 +1,52 @@
-/*----------------------------------------------------*/
-/* Quote Loop
------------------------------------------------------- */
+// Quote Loop
 
 function fade($ele) {
-	$ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
-			var $next = $(this).next('.quote');
-			fade($next.length > 0 ? $next : $(this).parent().children().first());
- });
+	$ele.fadeIn(1000).delay(3000).fadeOut(1000, function () {
+		var $next = $(this).next('.quote');
+		fade($next.length > 0 ? $next : $(this).parent().children().first());
+	});
 }
 fade($('.quoteLoop > .quote').first());
 
+// Navigation
 
-/*----------------------------------------------------*/
-/* Navigation
------------------------------------------------------- */
-
-$(window).scroll(function() {
-
+$(window).scroll(function () {
 	if ($(window).scrollTop() > 300) {
-			$('.main_nav').addClass('sticky');
+		$('.main_nav').addClass('sticky');
 	} else {
-			$('.main_nav').removeClass('sticky');
+		$('.main_nav').removeClass('sticky');
 	}
 });
 
 // Mobile Navigation
-$('.mobile-toggle').click(function() {
+$('.mobile-toggle').click(function () {
 	if ($('.main_nav').hasClass('open-nav')) {
-			$('.main_nav').removeClass('open-nav');
+		$('.main_nav').removeClass('open-nav');
 	} else {
-			$('.main_nav').addClass('open-nav');
+		$('.main_nav').addClass('open-nav');
 	}
 });
 
-$('.main_nav li a').click(function() {
+$('.main_nav li a').click(function () {
 	if ($('.main_nav').hasClass('open-nav')) {
-			$('.navigation').removeClass('open-nav');
-			$('.main_nav').removeClass('open-nav');
+		$('.navigation').removeClass('open-nav');
+		$('.main_nav').removeClass('open-nav');
 	}
 });
 
+// Smooth Scrolling
 
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
-
-jQuery(document).ready(function($) {
-
- $('.smoothscroll').on('click',function (e) {
+jQuery(document).ready(function ($) {
+	$('.smoothscroll').on('click', function (e) {
 		e.preventDefault();
-
 		var target = this.hash,
-		$target = $(target);
-
+			$target = $(target);
 		$('html, body').stop().animate({
-				'scrollTop': $target.offset().top
+			'scrollTop': $target.offset().top
 		}, 800, 'swing', function () {
-				window.location.hash = target;
+			window.location.hash = target;
 		});
+	});
 });
 
-});
-
-
-TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+TweenMax.staggerFrom(".heading", 0.8, { opacity: 0, y: 20, delay: 0.2 }, 0.4);
