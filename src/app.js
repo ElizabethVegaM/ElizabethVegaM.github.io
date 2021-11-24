be8835fe241f209f5f9e11a6f75ebea6a583d7ab
Elizabeth Vega <eb.vega.m@gmail.com>
2021-11-24 17:14:19 -0300

    styles: contact and responsive design

--- src/app.js
+++ src/app.js
@@ -1,12 +1,3 @@
-// Quote Loop
-
-function fade($ele) {
-	$ele.fadeIn(1000).delay(3000).fadeOut(1000, function () {
-		var $next = $(this).next('.quote');
-		fade($next.length > 0 ? $next : $(this).parent().children().first());
-	});
-}
-fade($('.quoteLoop > .quote').first());
 
 // Navigation
 
